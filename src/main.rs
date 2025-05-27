1c1d22520ace2c5aaceb964d8f67c15f288e0ae9
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-05-27 09:36:03 +0200

    feat: Add basic Campionato endpoint with Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::serde::{Deserialize, Serialize, json::Json};
+use rocket::{get, launch, routes};
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_campionato])
 }
+
+
+#[derive(Serialize)]
+pub struct Campionato {
+    year: u32,
+    scorer: String,
+    coach: String,
+}
+
+// A function that returns your Campionato struct as JSON
+
+#[get("/")]
+fn get_campionato() -> Json<Campionato> {
+    Json(Campionato {
+        year: 2010,
+        scorer: "Del Piero".to_string(),
+        coach: "Antonio Conte".to_string(),
+    })
+}
+
