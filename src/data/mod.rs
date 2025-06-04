49525b1d686347ff1f46eccf395ea8bc59369f2e
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-06-04 09:12:08 +0200

    Expanded championship data with 2015-2019 Scudetti

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -7,11 +7,37 @@ pub fn get_all_scudetti() -> Vec<Campionato> {
         scorer: "Del Piero".to_string(),
         coach: "Antonio Conte".to_string(),
         });
+    
     all_scudetti.push( Campionato {
         year: 2020,
         scorer: "Ronaldo".to_string(),
         coach: "Pirlo".to_string(),
+        });
+    
+    all_scudetti.push( Campionato {
+        year: 2019,
+        scorer: "Ronaldo".to_string(),
+        coach: "Sarri".to_string(),
     });
+    
+    all_scudetti.push( Campionato {
+        year: 2018,
+        scorer: "Ronaldo".to_string(),
+        coach: "Allegri".to_string(),
+    });
+
+    all_scudetti.push( Campionato {
+        year: 2017,
+        scorer: "Higuain".to_string(),
+        coach: "Allegri".to_string(),
+    });
+
+    all_scudetti.push( Campionato {
+        year: 2016,
+        scorer: "Higuain".to_string(),
+        coach: "Allegri".to_string(),
+    });
+    
 
     all_scudetti
 }
